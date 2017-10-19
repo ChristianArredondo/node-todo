f479274b10890805a5011662a15090e327c9b544
christianarredondo <christianarredondo@utexas.edu>
2017-10-18 21:12:17 -0500

    add PATCH method at /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
+const _ = require('lodash');
 const {ObjectID} = require('mongodb');
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo}  = require('./models/todo');
@@ -58,6 +59,33 @@ app.delete('/todos/:id', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Started up at ${port}`);
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then(todo => {
+    if (!todo){
+      return res.status(400).send();
+    }
+
+    res.send({todo});
+   }).catch(e => {
+     console.log(e);
+    res.status(400).send();
+  })
 });
 
 module.exports = {app};
