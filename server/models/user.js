5557d8b571e3a852ee1193d1959857dcabd2e73c
christianarredondo <christianarredondo@utexas.edu>
2017-10-20 21:36:56 -0500

    post users route with token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
@@ -33,5 +35,27 @@ var User = mongoose.model('User', {
     }
   ]
 });
+
+// Called automatically by JSON.stringify
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  // converts mongoose object to regular schema object for existing props
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123ROFL').toString();
+  user.tokens.push({access, token});
+  
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};  
